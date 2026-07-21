97188b89de7cca4fae1a9d49486db823333887fc
altexsoft-anton-palchyk <anton.palchyk@altexsoft.com>
2026-07-21 12:00:35 +0300

    fix authenticated header state and styling

--- web/src/app/layout/app-header/app-header.component.ts
+++ web/src/app/layout/app-header/app-header.component.ts
@@ -1,12 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../auth.service';
+import { AppIconComponent } from '../../shared/app-icon.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [AppIconComponent, RouterLink],
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppHeaderComponent {}
+export class AppHeaderComponent {
+  private readonly auth = inject(AuthService);
+
+  readonly isLoggedIn = computed(() => Boolean(this.auth.currentUserId()));
+}
